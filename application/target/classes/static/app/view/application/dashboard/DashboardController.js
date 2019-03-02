34449967fa72d2ced4405a15ee3f1f8c625d5c7f
gonghr <13962217@qq.com>
2019-03-02 14:08:27 +0800

    删除不必要的文件

--- application/target/classes/static/app/view/application/dashboard/DashboardController.js
+++
@@ -1,8 +0,0 @@
-Ext.define('AM.view.application.dashboard.DashboardController', {
-	extend: 'Ext.app.ViewController',
-	requires: [
-		// 'AM.model.main.Resource'
-	]
-	,alias: 'controller.application.dashboard.DashboardController'
-
-})
--- application/target/classes/static/resources/js/application.js
+++
@@ -1,2 +0,0 @@
-var global = {url:''}
-Ext.dashboard=[]
